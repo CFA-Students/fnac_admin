c8b2ac6d9fc3f5b275f7f2b7587ddc796d71e61d
E-jarod <ejilane.jarod@gmail.com>
2021-04-06 23:34:50 +0200

    null safe

--- lib/app_bloc_observer.dart
+++ lib/app_bloc_observer.dart
@@ -2,20 +2,20 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 
 class AppBlocObserver extends BlocObserver {
   @override
-  void onError(Cubit cubit, Object error, StackTrace stacktrace) {
-    super.onError(cubit, error, stacktrace);
-    print('${cubit.runtimeType} $error | $stacktrace');
+  void onError(BlocBase blocBase, Object error, StackTrace stacktrace) {
+    super.onError(blocBase, error, stacktrace);
+    print('${blocBase.runtimeType} $error | $stacktrace');
   }
 
   @override
-  void onEvent(Cubit cubit, Object event) {
-    super.onEvent(cubit, event);
+  void onEvent(Bloc<dynamic, dynamic> bloc, Object event) {
+    super.onEvent(bloc, event);
     print(event);
   }
 
   @override
-  void onTransition(Cubit cubit, Transition transition) {
-    super.onTransition(cubit, transition);
+  void onTransition(Bloc<dynamic, dynamic> bloc, Transition transition) {
+    super.onTransition(bloc, transition);
     print(transition);
   }
 }
